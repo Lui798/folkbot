88816b8b881b1c1de40c9d1681ab2873509f394e
Lui798 <42649201+Lui798@users.noreply.github.com>
2019-10-12 15:19:15 -0500

    fix setfooter

--- src/main/java/lui798/folkbot/command/EmbedCommand.java
+++ src/main/java/lui798/folkbot/command/EmbedCommand.java
@@ -43,7 +43,9 @@ public class EmbedCommand extends Command {
                         builder.setTitle(parts.get(1));
                     break;
                 case "setFooter":
-                    if (parts.get(2).equals("guildIcon"))
+                    if (parts.size() < 2)
+                        builder.setFooter(parts.get(1));
+                    else if (parts.get(2).equals("guildIcon"))
                         builder.setFooter(parts.get(1), message.getGuild().getIconUrl());
                     else
                         builder.setFooter(parts.get(1), parts.get(2));
